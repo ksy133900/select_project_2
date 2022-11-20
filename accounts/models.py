238db1ffac966ac8e97417eb0404966225e8cf20
ksy133900 <tkdgns13578@gmail.com>
2022-11-20 15:50:06 +0900

    회원정보 프로필 이미지 수정

--- accounts/models.py
+++ accounts/models.py
@@ -1,13 +1,15 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 from imagekit.models import ProcessedImageField
-from imagekit.processors import Thumbnail
+from imagekit.processors import ResizeToFill
 from django.conf import settings
 
 # Create your models here.
 class User(AbstractUser):
     nickname = models.CharField(max_length=20, unique=True, null=True)
-    followings = models.ManyToManyField('self', symmetrical=False, related_name='followers')
+    followings = models.ManyToManyField(
+        "self", symmetrical=False, related_name="followers"
+    )
 
     def __str__(self):
         return self.email
@@ -15,14 +17,14 @@ class User(AbstractUser):
 
 class Profile(models.Model):
     user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
-    intro = models.TextField(blank=True)    # 소개글
+    intro = models.TextField(blank=True)  # 소개글
     image = ProcessedImageField(
-                blank = True,
-                upload_to = 'profile/',
-                processors = [Thumbnail(300, 300)],
-                format = 'JPEG',
-                options = {'quality': 60},
-    		)
+        blank=True,
+        upload_to="profile/",
+        processors=[ResizeToFill(300, 300)],
+        format="JPEG",
+        options={"quality": 60},
+    )
 
     def __str__(self):
         return self.user.email
